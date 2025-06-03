6f3f97680bf29c62bfd427935df7192e315739e0
rathodvatsalraj <vatsalrajrathod@gmail.com>
2025-06-03 11:04:24 +0000

    learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 12345
+let accountEmail = "vatalrajgmail"
+var accountPassword = "12345"
+accountCity = "Rajkot"
+
+
+/*
+prefer not to use var
+because of issue in block scpoe and funtional scope
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
+
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node JS not the browser
+
+let name = "vatsal"
+let age = 18
+let isLoggedIn = true
+let state;
+
+
+console.log(typeof state); // undefined
+console.log(typeof null); // object
+
